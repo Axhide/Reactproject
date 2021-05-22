d94060ed5f58e357ade16ab165cbe9d8507321ca
Axhide <axhide1@gmail.com>
2021-05-22 22:30:11 +0300

    lesson 57

---
+++ src/components/common/preloader/Preloader.js
@@ -0,0 +1,8 @@
+import preloader from "../../../assets/images/Reload-0.6s-114px (1).svg";
+import React from "react";
+
+let Preloader = () => {
+    return <div><img src={preloader}/></div>
+}
+
+export default Preloader
